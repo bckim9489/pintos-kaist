ebec0e6c8600009fb5c5163962df677682473423
Ubuntu <ubuntu@ip-172-31-33-113.ap-northeast-2.compute.internal>
2023-12-19 05:33:50 +0000

    Project 3 VM init

--- include/vm/vm.h
+++ include/vm/vm.h
@@ -2,6 +2,7 @@
 #define VM_VM_H
 #include <stdbool.h>
 #include "threads/palloc.h"
+#include <kernel/hash.h>
 
 enum vm_type {
 	/* page not initialized */
@@ -46,6 +47,7 @@ struct page {
 	struct frame *frame;   /* Back reference for frame */
 
 	/* Your implementation */
+	struct hash_elem hash_elem;
 
 	/* Per-type data are binded into the union.
 	 * Each function automatically detects the current union */
@@ -63,6 +65,7 @@ struct page {
 struct frame {
 	void *kva;
 	struct page *page;
+	struct list_elem frame_elem;
 };
 
 /* The function table for page operations.
@@ -85,6 +88,8 @@ struct page_operations {
  * We don't want to force you to obey any specific design for this struct.
  * All designs up to you for this. */
 struct supplemental_page_table {
+	struct hash spt_hash;
+
 };
 
 #include "threads/thread.h"
@@ -108,5 +113,5 @@ bool vm_alloc_page_with_initializer (enum vm_type type, void *upage,
 void vm_dealloc_page (struct page *page);
 bool vm_claim_page (void *va);
 enum vm_type page_get_type (struct page *page);
-
+unsigned page_hash (const struct hash_elem *page, void *aux UNUSED);
 #endif  /* VM_VM_H */
